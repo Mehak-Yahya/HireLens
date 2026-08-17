5ae0489e0302aa2190ffdaecfe3c86c3b7d127b3
Mehak Yahya <kandhirmehak@gmail.com>
2026-08-17 22:42:22 +0500

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,26 @@
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB connected: ${connection.connection.host}`);
+    console.log(
+      `MongoDB connected: ${connection.connection.host}`
+    );
+
+    console.log(
+      `MongoDB database: ${connection.connection.name}`
+    );
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
+    console.error(
+      "MongoDB connection failed:",
+      error.message
+    );
+
     process.exit(1);
   }
 };
 
 export default connectDB;
+
